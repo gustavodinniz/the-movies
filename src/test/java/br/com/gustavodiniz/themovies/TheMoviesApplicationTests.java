6438914a256ae1eff065fb2a8a0549cf04b0a4c0
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-06-08 10:11:40 -0300

    initial project the-movies

---
+++ src/test/java/br/com/gustavodiniz/themovies/TheMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavodiniz.themovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
