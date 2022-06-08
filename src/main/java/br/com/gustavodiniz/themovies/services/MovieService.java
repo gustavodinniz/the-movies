2d4f659ef5f74becd1285920452b34c2fd04eb8e
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-06-08 16:14:24 -0300

    remove imports

--- src/main/java/br/com/gustavodiniz/themovies/services/MovieService.java
+++ src/main/java/br/com/gustavodiniz/themovies/services/MovieService.java
@@ -1,13 +1,9 @@
 package br.com.gustavodiniz.themovies.services;
 
-import br.com.gustavodiniz.themovies.dtos.MovieResponse;
 import br.com.gustavodiniz.themovies.dtos.TheMovieDbApiResponse;
 import br.com.gustavodiniz.themovies.models.MovieModel;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
-import java.util.Arrays;
-import java.util.List;
 
 public interface MovieService {
     TheMovieDbApiResponse findTopRated(String apiKey, String language, Long page);
